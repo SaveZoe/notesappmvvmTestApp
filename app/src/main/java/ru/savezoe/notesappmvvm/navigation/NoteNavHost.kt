20690fa7030cc8019383a7217bb0cc06dee129a7
SaveZoe <vepsawl@gmail.com>
2022-04-13 15:59:40 +0300

    Test_One

---
+++ app/src/main/java/ru/savezoe/notesappmvvm/navigation/NoteNavHost.kt
@@ -0,0 +1,29 @@
+package ru.savezoe.notesappmvvm.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ru.savezoe.notesappmvvm.screens.Add
+import ru.savezoe.notesappmvvm.screens.Main
+import ru.savezoe.notesappmvvm.screens.Note
+import ru.savezoe.notesappmvvm.screens.Start
+
+sealed class NavRoute(val route: String) {
+    object Start : NavRoute("start_screen")
+    object Main : NavRoute("main_screen")
+    object Add : NavRoute("add_screen")
+    object Note : NavRoute("note_screen")
+}
+
+@Composable
+fun NoteNavHost() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = NavRoute.Start.route) {
+        composable(NavRoute.Start.route) { Start(navController = navController) }
+        composable(NavRoute.Main.route) { Main(navController = navController) }
+        composable(NavRoute.Add.route) { Add(navController = navController) }
+        composable(NavRoute.Note.route) { Note(navController = navController) }
+    }
+}
